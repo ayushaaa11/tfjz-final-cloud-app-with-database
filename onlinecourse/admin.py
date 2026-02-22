50673057ffe8d9c8f95248d077fd65ed691ce756
ayushaaa11 <ayusha.ananya@gmail.com>
2026-02-22 05:53:01 -0800

    Update admin.py

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -1,48 +1,41 @@
 from django.contrib import admin
+from .models import Course, Lesson, Question, Choice, Enrollment, Submission, User
 
-# Register your models here.
-from django.contrib import admin
-# from django.db import models
-# <HINT> Import any new Models here
-from .models import Course, Lesson, Instructor, Learner, Question, Choice
 
-# <HINT> Register QuestionInline and ChoiceInline classes here
-class LessonInline(admin.StackedInline):
-    model = Lesson
-    extra = 5
-
-# Add inline class below>>
-class QuestionInline(admin.StackedInline):
-    model = Question
-    extra = 5
-class ChoiceInline(admin.StackedInline):
+# Inline for Choice inside Question
+class ChoiceInline(admin.TabularInline):
     model = Choice
-    extra = 5
+    extra = 2
 
-# Register your models here.
-class CourseAdmin(admin.ModelAdmin):
-    inlines = [LessonInline]
-    list_display = ('name', 'pub_date')
-    list_filter = ['pub_date']
-    search_fields = ['name', 'description']
 
-class LessonAdmin(admin.ModelAdmin):
-    inlines = [QuestionInline]
-    list_display = ['course', 'title']
+# Inline for Question inside Lesson
+class QuestionInline(admin.TabularInline):
+    model = Question
+    extra = 1
 
-# Add class model admin below>>
+
+# Question Admin
 class QuestionAdmin(admin.ModelAdmin):
     inlines = [ChoiceInline]
-    list_display = ['question_text', 'lesson']
+    list_display = ('question_text', 'lesson')
 
-class ChoiceAdmin(admin.ModelAdmin):
-    list_display = ['question', 'lesson']
 
-# <HINT> Register Question and Choice models here
+# Lesson Admin
+class LessonAdmin(admin.ModelAdmin):
+    inlines = [QuestionInline]
+    list_display = ('title', 'course')
 
+
+# Course Admin
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
+
+
+# Register models
 admin.site.register(Course, CourseAdmin)
 admin.site.register(Lesson, LessonAdmin)
-admin.site.register(Instructor)
-admin.site.register(Learner)
 admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
+admin.site.register(Enrollment)
+admin.site.register(Submission)
+admin.site.register(User)
